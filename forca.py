6ca5759d94fb39c14977938d34ff9743f5cae7ff
Alexander <alxv.oliveira@gmail.com>
2019-09-20 10:19:46 -0300

    Adicionando identificador de letras da palavra secreta

--- forca.py
+++ forca.py
@@ -3,14 +3,22 @@ def jogar():
     print("Bem vindo ao jogo da Forca")
     print("*********************************")
 
-    print("Fim do jogo")
-
     palavra_secreta = "banana"
     enforcou = False
     acertou = False
 
-    while(not enforcou and not acertou):
+    while(not acertou and not enforcou):
+        chute = input("Qual letra? ")
+        index = 0
+
+        for letra in palavra_secreta:
+            if(chute == letra):
+                print("Encontrei a letra '{}' na posição {}".format(chute, index))
+            index += 1
+
         print("Jogando ... ")
+
+    print("Fim do jogo")
 
 if(__name__ == "__main__"):
     jogar()
